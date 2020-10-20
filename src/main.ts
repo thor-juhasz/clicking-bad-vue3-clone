8fbc313fc53071f2239fe1ee5687a7e68e71a09e
Thor Juhasz <thor@juhasz.pro>
2020-10-20 23:34:51 +0200

    Use vue-gtag-next for analytics/tag-manager

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
-import { createApp } from 'vue'
+import { createApp } from "vue"
+import VueGtag from "vue-gtag-next"
 
-import router from './router'
+import router from "./router"
+import App from "./App.vue"
 
-import App from './App.vue'
+const vm = createApp(App)
 
-createApp(App).use(router).mount('#app')
+if (process.env.VUE_APP_GA_ID) {
+    vm.use(VueGtag, {
+        config: { id: process.env.VUE_APP_GA_ID },
+    }, router)
+}
+
+vm.use(router).mount('#app')
